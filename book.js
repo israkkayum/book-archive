303c266131774db1a823d732df36f1c85cb1819a
israkkayum <israk.kayum@gmail.com>
2021-09-02 17:18:08 +0600

    second commit

--- book.js
+++ book.js
@@ -2,13 +2,20 @@ const searchBook = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     searchField.value = '';
+    loddingSpiner('block');
 
-    const url = `http://openlibrary.org/search.json?q=${searchText}`;
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
     fetch(url)
       .then(res => res.json())
       .then(data => showResult(data.docs));
 
 };
+
+// Spinner
+const loddingSpiner = displayStyle => {
+    document.getElementById('spinner').style.display = displayStyle;
+};
+
 
 const showResult = books => {
     const searchResult = document.getElementById('search-result');
@@ -21,11 +28,14 @@ const showResult = books => {
     matchingResult.innerText = 0;
 
     let counter = 0;
+
     // For each loop
     books.forEach(book => {
+
       // Matching result count
        counter++;
        matchingResult.innerText = counter;
+
       //  Show result
        const div = document.createElement('div');
        div.innerHTML = `
@@ -37,19 +47,23 @@ const showResult = books => {
               <div class="col-md-8">
                 <div class="card-body">
                   <h5 class="card-title">${book.title}</h5>
-                  <p class="card-text">By: ${book.author_name}</p>
-                  <p class="card-text">Publisher: ${book.publisher}</p>
+                  <p class="card-text">By ${book.author_name}</p>
+                  <p class="card-text">Publisher : ${book.publisher}</p>
                   <p class="card-text"><small class="text-muted">First published in ${book.publish_date[0]}</small></p>
                 </div>
               </div>
             </div>
           </div>`;
+          
         searchResult.appendChild(div);
+        loddingSpiner('none');
       });
+
       // Show no found any result message
       if (counter === 0) {
         const div = document.createElement('div');
         div.innerHTML = `<img src="logo/no-result-found.gif" class="img-fluid rounded-start w-auto h-auto" alt="...">`;
         noResultFound.appendChild(div);
-      }
-}
+        loddingSpiner('none');
+      };
+};
